f5a785fbe4aaad5ab286987b3d779a5e5a5af727
Farrokh Poorooshasb <fa386@yahoo.com>
2014-08-05 18:36:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFixedupapp::Application.config.secret_token = '706497e0ac31697a7c536c3bd08777fefdb449c3f01979f9c6b47e52ecfad7f653095be743f33b87390fa29912db0acbb008945db8eec972d387ae0f599892ee'
